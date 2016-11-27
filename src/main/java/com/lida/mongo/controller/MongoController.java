980ba98fe6acb03d7dbdcdda330046ef81642644
quanzongwei <252260070@qq.com>
2016-11-27 14:30:00 +0800

    add:小修改

--- src/main/java/com/lida/mongo/controller/MongoController.java
+++ src/main/java/com/lida/mongo/controller/MongoController.java
@@ -6,9 +6,13 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 /**
  * Created by DuLida on 2016/10/20.
@@ -20,20 +24,21 @@ public class MongoController {
     @Autowired
     PersonDao personDao;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String go() {
         return "mogoList";
     }
 
-
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public String go2() {
-        //findALl
+        // findALl
         System.out.println(personDao.findAll());
 
         return "mogoList";
     }
-
 
     @RequestMapping(value = "/findIdNameGe", method = RequestMethod.GET)
     public String go23() {
@@ -56,24 +61,32 @@ public class MongoController {
 
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public String go6() {
-//        System.out.println(personDao.findByName("name0"));
-       System.out.println(personDao.findByName("name0", new PageRequest(0, 10, Sort.Direction.ASC, "_id")));
+        // System.out.println(personDao.findByName("name0"));
+        System.out.println(personDao.findByName("name0", new PageRequest(0, 10, Sort.Direction.ASC, "_id")));
 
         return "mogoList";
     }
 
-    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String go7() {
-//        System.out.println(personDao.findByName("name0"));
-       System.out.println(personDao.deleteByName("name9"));
-       System.out.println(personDao.deletePersonByName("name8"));
+        // System.out.println(personDao.findByName("name0"));
+        System.out.println(personDao.deleteByName("name9"));
+        System.out.println(personDao.deletePersonByName("name8"));
 
-        //更新操作成功,保存就是更新了
+        // 更新操作成功,保存就是更新了
         Person person = personDao.findOne(new ObjectId("5836dc7b793a34373068ff9c"));
         System.out.println(person);
         person.setAge(100);
         System.out.println(personDao.save(person));
 
+        return "mogoList";
+    }
+
+    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
+    public String go8() {
+
+        // 查找一个
+        System.out.println(mongoTemplate.findOne(new Query(where("name").is("name0")), Person.class));
         return "mogoList";
     }
 
--- src/main/java/com/lida/mongo/mongoDao/PersonMongoImpl.java
+++ src/main/java/com/lida/mongo/mongoDao/PersonMongoImpl.java
@@ -1,17 +1,16 @@
 package com.lida.mongo.mongoDao;
 
-import com.lida.mongo.entity.Address;
 import com.lida.mongo.entity.Person;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 /**
  * Created by DuLida on 2016/10/21.
@@ -34,16 +33,22 @@ public class PersonMongoImpl implements PersonMongoDao {
 
     @Override
     public void removePerson(String userName) {
-        mongoTemplate.remove(Query.query(Criteria.where("name").is(userName)),"person");
+        mongoTemplate.remove(Query.query(where("name").is(userName)), "person");
     }
 
     @Override
     public void updatePerson() {
-        mongoTemplate.updateMulti(Query.query(Criteria.where("age").gt(3).lte(5)), Update.update("age",3),"person");
+        mongoTemplate.updateMulti(Query.query(where("age").gt(3).lte(5)), Update.update("age", 3), "person");
     }
 
     @Override
     public List<Person> findForRequery(String userName) {
-        return mongoTemplate.find(Query.query(Criteria.where("name").is(userName)),Person.class);
+        return mongoTemplate.find(Query.query(where("name").is(userName)),Person.class);
+    }
+
+    public void func() {
+        MongoOperations mongoOperations = mongoTemplate;
+        System.out.println(mongoOperations.findOne(new Query(where("name").is("name0")), Person.class));
+
     }
 }
